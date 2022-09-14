18c1dc3b5b84f327400a200efab741130d944bbf
Julio Faracco <jcfaracco@gmail.com>
2022-09-14 01:32:58 -0300

    Add new dataset generator called make_blobs
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

---
+++ dasf/datasets/blobs.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from sklearn.datasets import make_blobs as make_blobs_CPU
+from dask_ml.datasets import make_blobs as make_blobs_MCPU
+
+try:
+    from cuml.datasets import make_blobs as make_blobs_GPU
+    from cuml.dask.datasets import make_blobs as make_blobs_MGPU
+except ImportError:
+    pass
+
+from dasf.utils.utils import is_gpu_supported
+from dasf.utils.utils import is_dask_supported
+from dasf.utils.utils import is_dask_gpu_supported
+
+
+class make_blobs:
+    def __new__(cls, **kwargs):
+        instance = super().__new__(cls)
+        if kwargs is None:
+            return instance
+        else:
+            return instance(**kwargs)
+
+    def _lazy_make_blobs_cpu(self, **kwargs):
+        return make_blobs_MCPU(**kwargs)
+
+    def _lazy_make_blobs_gpu(self, **kwargs):
+        return make_blobs_MGPU(**kwargs)
+
+    def _make_blobs_cpu(self, **kwargs):
+        return make_blobs_CPU(**kwargs)
+
+    def _make_blobs_gpu(self, **kwargs):
+        return make_blobs_GPU(**kwargs)
+
+    def __call__(self, **kwargs):
+        if is_dask_gpu_supported():
+            return self._lazy_make_blobs_gpu(**kwargs)
+        elif is_dask_supported():
+            return self._lazy_make_blobs_cpu(**kwargs)
+        elif is_gpu_supported():
+            return self._make_blobs_gpu(**kwargs)
+        else:
+            return self._make_blobs_cpu(**kwargs)
