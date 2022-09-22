bd29db4a9217e77b3602022a1665a318f503d6d9
root <jcfaracco@gmail.com>
2022-09-22 04:20:49 +0000

    Adding decorators to fetch data from devices
    Signed-off-by: root <jcfaracco@gmail.com>

--- dasf/utils/decorators.py
+++ dasf/utils/decorators.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from dasf.utils.types import is_dask_array
 from dasf.utils.utils import is_gpu_supported
 from dasf.utils.utils import is_dask_supported
 from dasf.utils.utils import is_dask_gpu_supported
@@ -28,5 +29,49 @@ def task_handler(func):
             return getattr(cls, wrapper_func_attr)(*new_args)
         else:
             return func(*new_args)
+
+    return wrapper
+
+
+def fetch_args_from_dask(func):
+    def wrapper(*args, **kwargs):
+        new_kwargs = dict()
+        new_args = []
+
+        for k, v in kwargs.items():
+            if is_dask_array(v):
+                new_kwargs[k] = v.compute()
+            else:
+                new_kwargs[k] = v
+
+        for v in args:
+            if is_dask_array(v):
+                new_args.append(v.compute())
+            else:
+                new_args.append(v)
+
+        return func(*new_args, **new_kwargs)
+
+    return wrapper
+
+
+def fetch_args_from_gpu(func):
+    def wrapper(*args, **kwargs):
+        new_kwargs = dict()
+        new_args = []
+
+        for k, v in kwargs.items():
+            if is_dask_array(v):
+                new_kwargs[k] = v.get()
+            else:
+                new_kwargs[k] = v
+
+        for v in args:
+            if is_dask_array(v):
+                new_args.append(v.get())
+            else:
+                new_args.append(v)
+
+        return func(*new_args, **new_kwargs)
 
     return wrapper
