f438c0e66154808af557b014bfc4f812b9c84f88
Julio Faracco <jcfaracco@gmail.com>
2022-09-23 03:12:21 +0000

    Adding AgglomerativeClustering into __init__.py
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- dasf/ml/cluster/__init__.py
+++ dasf/ml/cluster/__init__.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
+from dasf.ml.cluster.agglomerative import AgglomerativeClustering  # noqa
 from dasf.ml.cluster.kmeans import KMeans  # noqa
 from dasf.ml.cluster.hdbscan import HDBSCAN  # noqa
 from dasf.ml.cluster.dbscan import DBSCAN  # noqa
 from dasf.ml.cluster.som import SOM  # noqa
 
 
-cluster_methods = ["KMeans", "HDBSCAN", "DBSCAN", "SOM"]
+cluster_methods = ["AgglomerativeClustering",
+                   "KMeans",
+                   "HDBSCAN",
+                   "DBSCAN",
+                   "SOM"]
 
 __all__ = cluster_methods
