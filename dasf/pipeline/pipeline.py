305415e2c9d96297264df0d19b11a3dd21a6ccaf
Julio Faracco <jcfaracco@gmail.com>
2022-09-24 13:24:37 -0300

    Fixing wrong exception message inside pipeline
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- dasf/pipeline/pipeline.py
+++ dasf/pipeline/pipeline.py
@@ -75,9 +75,9 @@ class Pipeline:
         elif hasattr(obj, "fit"):
             self.__add_into_dag(obj.fit, kwargs, obj)
         else:
-            raise NotImplementedError(
-                f"There is no implementation of {wrapper_func_attr} nor "
-                 "{func_name}"
+            raise ValueError(
+                f"This object {obj.__name__} is not a function, method "
+                 "or a transformer object."
             )
 
         return self
