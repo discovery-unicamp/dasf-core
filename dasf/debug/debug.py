840c18c7db3236a353ed48c2adc9aa606c838e22
Julio Faracco <jcfaracco@gmail.com>
2022-09-29 04:17:27 +0000

    Fixing debug objects
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- dasf/debug/debug.py
+++ dasf/debug/debug.py
@@ -6,23 +6,16 @@ import dask.dataframe as ddf
 from IPython.core.display import HTML as iHTML
 from IPython.core.display import display as idisplay
 
-from dasf.pipeline import Operator
+from dasf.utils.types import is_dask_array
+from dasf.utils.types import is_dask_dataframe
 
 
-class Debug(Operator):
-    def __init__(self, name=None, **kwargs):
-        self.__name = name
-
-        if name is None:
-            self.__name = "Debug"
-
-        super().__init__(name=self.__name, **kwargs)
-
-    def run(self, X):
+class Debug:
+    def display(self, X):
         if hasattr(X, "shape"):
             print("Datashape is:", X.shape)
 
-        if isinstance(X, da.core.Array) or isinstance(X, ddf.core.DataFrame):
+        if is_dask_array(X) or is_dask_dataframe(X):
             idisplay(iHTML(X._repr_html_()))
         else:
             print("Datatype is:", type(X))
@@ -31,14 +24,12 @@ class Debug(Operator):
         return X
 
 
-class VisualizeDaskData(Operator):
-    def __init__(self, filename=None, **kwargs):
-        super().__init__(name="Visualize Dask Data", **kwargs)
-
+class VisualizeDaskData:
+    def __init__(self, filename=None):
         self.filename = filename
 
-    def run(self, X):
-        if not isinstance(X, da.core.Array) and not isinstance(X, ddf.core.DataFrame):
+    def display(self, X):
+        if not is_dask_array(X) and not is_dask_dataframe(X):
             self.logger.warning("This is not a Dask element.")
             return X
 
