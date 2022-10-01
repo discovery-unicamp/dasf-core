7fd62237ded3b2db5aa715855426a2d6f8523190
Julio Faracco <jcfaracco@gmail.com>
2022-10-01 00:35:35 -0300

    Adding a workaround to fix CVE-2022-21797
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- dasf/ml/cluster/hdbscan.py
+++ dasf/ml/cluster/hdbscan.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# XXX: Import due to CVE-2022-21797
+import joblib
+import packaging
+
 from hdbscan import HDBSCAN as HDBSCAN_CPU
 
 from dasf.ml.cluster.classifier import ClusterClassifier
@@ -32,34 +36,46 @@ class HDBSCAN(ClusterClassifier):
         self.min_samples = min_samples
         self.p = p
 
-        self.__hdbscan_cpu = HDBSCAN_CPU(
-            alpha=self.alpha,
-            gen_min_span_tree=self.gen_min_span_tree,
-            leaf_size=self.leaf_size,
-            metric=self.metric,
-            min_cluster_size=self.min_cluster_size,
-            min_samples=self.min_samples,
-            p=self.p,
-        )
-
-        if is_gpu_supported():
-            self.__hdbscan_gpu = HDBSCAN_GPU(
+        if version.parse(joblib.__version__) >= version.parse("1.2.0")
+            self.__hdbscan_cpu = HDBSCAN_CPU(
                 alpha=self.alpha,
                 gen_min_span_tree=self.gen_min_span_tree,
+                leaf_size=self.leaf_size,
                 metric=self.metric,
                 min_cluster_size=self.min_cluster_size,
                 min_samples=self.min_samples,
                 p=self.p,
             )
 
+            if is_gpu_supported():
+                self.__hdbscan_gpu = HDBSCAN_GPU(
+                    alpha=self.alpha,
+                    gen_min_span_tree=self.gen_min_span_tree,
+                    metric=self.metric,
+                    min_cluster_size=self.min_cluster_size,
+                    min_samples=self.min_samples,
+                    p=self.p,
+                )
+        else:
+            self.__hdbscan_cpu = None
+            self.__hdbscan_gpu = None
+
     def _fit_cpu(self, X, y=None):
+        if not self.__hdbscan_cpu:
+            raise NotImplementedError
         self.__hdbscan_cpu.fit(X=X, y=y)
 
     def _fit_gpu(self, X, y=None, convert_dtype=True):
+        if not self.__hdbscan_gpu:
+            raise NotImplementedError
         self.__hdbscan_gpu.fit(X=X, y=y, convert_dtype=convert_dtype)
 
     def _fit_predict_cpu(self, X, y=None):
+        if not self.__hdbscan_cpu:
+            raise NotImplementedError
         self.__hdbscan_cpu.fit_predict(X=X, y=y)
 
     def _fit_predict_gpu(self, X, y=None):
+        if not self.__hdbscan_gpu:
+            raise NotImplementedError
         self.__hdbscan_gpu.fit_predict(X=X, y=y)
