244968ce2eddd33de29dc06069579f15e037bef7
Julio Faracco <jcfaracco@gmail.com>
2022-10-01 01:32:31 -0300

    A V2 for joblib fix
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- dasf/ml/cluster/__init__.py
+++ dasf/ml/cluster/__init__.py
@@ -2,7 +2,6 @@
 
 from dasf.ml.cluster.agglomerative import AgglomerativeClustering  # noqa
 from dasf.ml.cluster.kmeans import KMeans  # noqa
-from dasf.ml.cluster.hdbscan import HDBSCAN  # noqa
 from dasf.ml.cluster.dbscan import DBSCAN  # noqa
 from dasf.ml.cluster.som import SOM  # noqa
 
@@ -10,9 +9,19 @@ from dasf.ml.cluster.som import SOM  # noqa
 cluster_methods = [
     "AgglomerativeClustering",
     "KMeans",
-    "HDBSCAN",
     "DBSCAN",
     "SOM"
 ]
+
+# XXX: Import due to CVE-2022-21797
+import joblib
+from packaging import version
+
+if version.parse(joblib.__version__) >= version.parse("1.2.0"):
+    # Do not include HDBSCAN while it is not safe
+    from dasf.ml.cluster.hdbscan import HDBSCAN  # noqa
+
+    cluster_methods.append("HDBSCAN")
+# End of workaround
 
 __all__ = cluster_methods
