abd93176e6805f82ea615c028cdd76b1c38f61e8
Julio Faracco <jcfaracco@gmail.com>
2022-11-02 22:41:52 -0300

    Adding positional arguments to server script
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- build/start_jupyter_server.sh
+++ build/start_jupyter_server.sh
@@ -1,5 +1,40 @@
+#!/bin/bash
+
 PORT=8891
 TYPE="gpu"
+
+function print_help() {
+    echo "Usage: ./build_docker.sh [ARCH_TYPE]"
+    echo ""
+    echo "Standard options:"
+    echo "    -h or -help                      Display this help and exit"
+    echo "    --device ARCH_TYPE:              Build a container based on arch type."
+    echo "                                     Use 'gpu' for GPU container-based architecture."
+    echo "                                     Use 'cpu' for CPU container-based architecture (default='$TYPE')."
+    echo "    --port PORT                      Use the prefered port to start the server (default='$PORT')."
+    echo ""
+}
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -h|--help)
+      print_help
+      exit 0
+      ;;
+    --device)
+      TYPE="$2"
+      shift
+      shift
+      ;;
+    --port|-p)
+      PORT="$2"
+      shift
+      shift
+      ;;
+    *)
+      break;
+  esac
+done
 
 function GET_CONTAINER_CMD() {
     OLDIFS="$IFS"
