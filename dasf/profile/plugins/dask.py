7f9c03e94ab124a1525f142ad147318cb866ae7a
Otavio Napoli <otavio.napoli@gmail.com>
2023-03-22 15:58:08 -0300

    Added more info to dask slices

--- dasf/profile/plugins/dask.py
+++ dasf/profile/plugins/dask.py
@@ -8,29 +8,52 @@ class TaskTimePlugin(WorkerPlugin):
     def setup(self, worker):
         self.worker = worker
         self.start_times = dict()
+        self.transfer_times = dict()
+        self.waiting_time = dict()
         self.hostname = socket.gethostname()
 
     def transition(self, key, start, finish, *args, **kwargs):
-        if finish == "executing":
+        if finish == "executing": # start execting     
             self.start_times[key] = time.time()
-        elif start == "executing" and key in self.start_times:
-            duration = time.time() - self.start_times[key]
-            # add_trace_complete(
-            #     name=key,
-            #     process_id=self.hostname,
-            #     thread_id=self.worker.name,
-            #     timestamp=self.start_times[key],
-            #     duration=duration,
-            #     category=["worker", "processing"],
-            #     data={"from": start, "to": finish},
-            # )
-
-            add_trace_complete(
-                name="Dask task",
-                process_id=self.hostname,
-                thread_id=f"worker-{self.worker.name}",
-                timestamp=self.start_times[key],
-                duration=duration,
-                category=["worker", "dask", "processing time"],
-                data={"from": start, "to": finish, "key": key},
-            )
+            if key in self.waiting_time: 
+                duration = time.time() - self.waiting_time[key]
+                add_trace_complete(
+                    name="Waiting",
+                    process_id=self.hostname,
+                    thread_id=f"worker-{self.worker.name}",
+                    timestamp=self.transfer_times[key],
+                    duration=duration,
+                    category=["worker", "dask", "waiting"],
+                    data={"from": start, "to": finish, "key": key},
+                )
+            
+        elif (start == "executing" or start == "long-running") and key in self.start_times: # end executing
+            if key in self.start_times:
+                duration = time.time() - self.start_times[key]
+                add_trace_complete(
+                    name="Processing",
+                    process_id=self.hostname,
+                    thread_id=f"worker-{self.worker.name}",
+                    timestamp=self.start_times[key],
+                    duration=duration,
+                    category=["worker", "dask", "processing"],
+                    data={"from": start, "to": finish, "key": key},
+                )
+        if finish == "fetch": # start transfer
+            self.transfer_times[key] = time.time()
+        elif start == "flight" and finish == "memory":
+            if key in self.transfer_times:
+                duration = time.time() - self.transfer_times[key]
+                add_trace_complete(
+                    name="Transfering",
+                    process_id=self.hostname,
+                    thread_id=f"worker-{self.worker.name}",
+                    timestamp=self.transfer_times[key],
+                    duration=duration,
+                    category=["worker", "dask", "transfering"],
+                    data={"from": start, "to": finish, "key": key},
+                )
+            
+        if finish == "waiting":
+            self.waiting_time[key] = time.time()    
+        
