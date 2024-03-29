74f49c9f5db8bb78b9ac182e11ce84688bfe3b06
Julio Faracco <jcfaracco@gmail.com>
2024-03-29 12:50:43 -0300

    Including tests for make_classification and make_regression
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- tests/datasets/test_datasets.py
+++ tests/datasets/test_datasets.py
@@ -17,6 +17,8 @@ from dasf.utils.types import is_dask_array
 from dasf.utils.types import is_dask_gpu_array
 
 from dasf.datasets import make_blobs
+from dasf.datasets import make_classification
+from dasf.datasets import make_regression
 
 
 class TestDatasets(unittest.TestCase):
@@ -75,6 +77,123 @@ class TestDatasets(unittest.TestCase):
             centers = [(-6, -6), (0, 0), (9, 1)]
             X, y = make_blobs(n_samples=n_samples, centers=centers,
                               shuffle=False, random_state=42)
+
+            self.assertTrue(is_dask_gpu_array(X))
+            self.assertTrue(is_dask_gpu_array(y))
+
+            # Compute everything to gracefully shutdown
+            client.close()
+
+    @patch('dasf.datasets.datasets.is_gpu_supported', Mock(return_value=False))
+    @patch('dasf.datasets.datasets.is_dask_supported', Mock(return_value=False))
+    @patch('dasf.datasets.datasets.is_dask_gpu_supported', Mock(return_value=False))
+    def test_make_classification_cpu(self):
+        n_samples = 500000
+
+        X, y = make_classification(n_samples=n_samples, n_classes=2,
+                                   shuffle=False, random_state=42)
+
+        self.assertTrue(is_cpu_array(X))
+        self.assertTrue(is_cpu_array(y))
+
+    @patch('dasf.datasets.datasets.is_gpu_supported', Mock(return_value=False))
+    @patch('dasf.datasets.datasets.is_dask_supported', Mock(return_value=True))
+    @patch('dasf.datasets.datasets.is_dask_gpu_supported', Mock(return_value=False))
+    def test_make_classification_mcpu(self):
+        n_samples = 500000
+
+        X, y = make_classification(n_samples=n_samples, n_classes=2, shuffle=False,
+                                   random_state=42, chunks=(5000))
+
+        self.assertTrue(is_dask_array(X))
+        self.assertTrue(is_dask_array(y))
+
+    @unittest.skipIf(not is_gpu_supported(),
+                     "not supported CUDA in this platform")
+    @patch('dasf.datasets.datasets.is_gpu_supported', Mock(return_value=True))
+    @patch('dasf.datasets.datasets.is_dask_supported', Mock(return_value=False))
+    @patch('dasf.datasets.datasets.is_dask_gpu_supported', Mock(return_value=False))
+    def test_make_classification_gpu(self):
+        n_samples = 500000
+
+        X, y = make_classification(n_samples=n_samples, n_classes=2,
+                                   shuffle=False, random_state=42)
+
+        self.assertTrue(is_gpu_array(X))
+        self.assertTrue(is_gpu_array(y))
+
+    @unittest.skipIf(not is_gpu_supported(),
+                     "not supported CUDA in this platform")
+    @patch('dasf.datasets.datasets.is_gpu_supported', Mock(return_value=False))
+    @patch('dasf.datasets.datasets.is_dask_supported', Mock(return_value=False))
+    @patch('dasf.datasets.datasets.is_dask_gpu_supported', Mock(return_value=True))
+    def test_make_classification_mgpu(self):
+        with LocalCUDACluster() as cluster:
+            client = Client(cluster)
+
+            n_samples = 500000
+
+            X, y = make_classification(n_samples=n_samples, n_classes=2,
+                                       shuffle=False, random_state=42)
+
+            self.assertTrue(is_dask_gpu_array(X))
+            self.assertTrue(is_dask_gpu_array(y))
+
+            # Compute everything to gracefully shutdown
+            client.close()
+
+    @patch('dasf.datasets.datasets.is_gpu_supported', Mock(return_value=False))
+    @patch('dasf.datasets.datasets.is_dask_supported', Mock(return_value=False))
+    @patch('dasf.datasets.datasets.is_dask_gpu_supported', Mock(return_value=False))
+    def test_make_regression_cpu(self):
+        n_samples = 500000
+
+        X, y = make_regression(n_samples=n_samples, n_features=2,
+                               noise=1, random_state=42)
+
+        self.assertTrue(is_cpu_array(X))
+        self.assertTrue(is_cpu_array(y))
+
+    @patch('dasf.datasets.datasets.is_gpu_supported', Mock(return_value=False))
+    @patch('dasf.datasets.datasets.is_dask_supported', Mock(return_value=True))
+    @patch('dasf.datasets.datasets.is_dask_gpu_supported', Mock(return_value=False))
+    def test_make_regression_mcpu(self):
+        n_samples = 500000
+
+        X, y = make_regression(n_samples=n_samples, n_features=2,
+                               noise=1, random_state=42,
+                               chunks=(5000, 2))
+
+        self.assertTrue(is_dask_array(X))
+        self.assertTrue(is_dask_array(y))
+
+    @unittest.skipIf(not is_gpu_supported(),
+                     "not supported CUDA in this platform")
+    @patch('dasf.datasets.datasets.is_gpu_supported', Mock(return_value=True))
+    @patch('dasf.datasets.datasets.is_dask_supported', Mock(return_value=False))
+    @patch('dasf.datasets.datasets.is_dask_gpu_supported', Mock(return_value=False))
+    def test_make_regression_gpu(self):
+        n_samples = 500000
+
+        X, y = make_regression(n_samples=n_samples, n_features=2,
+                               noise=1, random_state=42)
+
+        self.assertTrue(is_gpu_array(X))
+        self.assertTrue(is_gpu_array(y))
+
+    @unittest.skipIf(not is_gpu_supported(),
+                     "not supported CUDA in this platform")
+    @patch('dasf.datasets.datasets.is_gpu_supported', Mock(return_value=False))
+    @patch('dasf.datasets.datasets.is_dask_supported', Mock(return_value=False))
+    @patch('dasf.datasets.datasets.is_dask_gpu_supported', Mock(return_value=True))
+    def test_make_regression_mgpu(self):
+        with LocalCUDACluster() as cluster:
+            client = Client(cluster)
+
+            n_samples = 500000
+
+            X, y = make_regression(n_samples=n_samples, n_features=2,
+                                   noise=1, random_state=42)
 
             self.assertTrue(is_dask_gpu_array(X))
             self.assertTrue(is_dask_gpu_array(y))
