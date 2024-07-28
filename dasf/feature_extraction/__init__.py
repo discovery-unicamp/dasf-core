40c0d037cf9498cda02211ea75bf9ae1ae3b1a60
Julio Faracco <jcfaracco@gmail.com>
2024-07-27 23:42:35 -0300

    Also fix some references to transforms
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- dasf/feature_extraction/__init__.py
+++ dasf/feature_extraction/__init__.py
@@ -3,10 +3,10 @@
 from dasf.feature_extraction.histogram import Histogram  # noqa
 
 # from dasf.feature_extraction.transform import ConcatenateToDataframe  # noqa
-from dasf.feature_extraction.transform import ConcatenateToArray  # noqa
-from dasf.feature_extraction.transform import GetSubDataframe  # noqa
-from dasf.feature_extraction.transform import GetSubeCubeArray  # noqa
-from dasf.feature_extraction.transform import SampleDataframe  # noqa
+from dasf.feature_extraction.transforms import ConcatenateToArray  # noqa
+from dasf.feature_extraction.transforms import GetSubDataframe  # noqa
+from dasf.feature_extraction.transforms import GetSubeCubeArray  # noqa
+from dasf.feature_extraction.transforms import SampleDataframe  # noqa
 
 __all__ = [
     "ConcatenateToArray",
