d751917d0303ec9690be72e1c7b9f3fc4e80ca98
Julio Faracco <jfaracco@redhat.com>
2025-02-23 23:56:28 -0300

    Add more tests for neighbors module
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- dasf/ml/neighbors/neighbors.py
+++ dasf/ml/neighbors/neighbors.py
@@ -2,18 +2,58 @@
 
 """ NearestNeighbors algorithm module. """
 
+from sklearn.neighbors import KNeighborsClassifier as KNeighborsClassifier_CPU
 from sklearn.neighbors import NearestNeighbors as NearestNeighbors_CPU
 
-from dasf.transforms.base import Fit, GetParams, SetParams
+from dasf.transforms.base import Fit, GetParams, Predict, SetParams, Transform
+from dasf.utils.decorators import task_handler
 from dasf.utils.funcs import is_gpu_supported
 
 try:
+    from cuml.neighbors import KNeighborsClassifier as KNeighborsClassifier_GPU
     from cuml.neighbors import NearestNeighbors as NearestNeighbors_GPU
 except ImportError:
     pass
 
 
-class NearestNeighbors(Fit, GetParams, SetParams):
+class KNeighbors(Transform):
+    """
+    Class representing a KNeighbors operation of the pipeline.
+    """
+
+    def _kneighbors_cpu(self, X, **kwargs):
+        """
+        Respective immediate kneighbors mocked function for local CPU(s).
+        """
+        raise NotImplementedError
+
+    def _kneighbors_gpu(self, X, **kwargs):
+        """
+        Respective immediate kneighbors mocked function for local GPU(s).
+        """
+        raise NotImplementedError
+
+    def _lazy_kneighbors_cpu(self, X, **kwargs):
+        """
+        Respective lazy kneighbors mocked function for CPUs.
+        """
+        raise NotImplementedError
+
+    def _lazy_kneighbors_gpu(self, X, **kwargs):
+        """
+        Respective lazy kneighbors mocked function for GPUs.
+        """
+        raise NotImplementedError
+
+    @task_handler
+    def kneighbors(self, X, **kwargs):
+        """
+        Generic kneighbors funtion according executor.
+        """
+        ...
+
+
+class NearestNeighbors(Fit, GetParams, SetParams, KNeighbors):
     """
     Unsupervised learner for implementing neighbor searches.
 
@@ -227,3 +267,328 @@ class NearestNeighbors(Fit, GetParams, SetParams):
             Estimator instance.
         """
         return self.__nn_cpu.set_params(**params)
+
+    def _kneighbors_cpu(self, X, **kwargs):
+        """
+        Find the K-neighbors of a point using CPU.
+
+        Returns indices of and distances to the neighbors of each point.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_queries, n_features), \
+            or (n_queries, n_indexed) if metric == 'precomputed', default=None
+            The query point or points.
+            If not provided, neighbors of each indexed point are returned.
+            In this case, the query point is not considered its own neighbor.
+
+        n_neighbors : int, default=None
+            Number of neighbors required for each sample. The default is the
+            value passed to the constructor.
+
+        return_distance : bool, default=True
+            Whether or not to return the distances.
+
+        Returns
+        -------
+        neigh_dist : ndarray of shape (n_queries, n_neighbors)
+            Array representing the lengths to points, only present if
+            return_distance=True.
+
+        neigh_ind : ndarray of shape (n_queries, n_neighbors)
+            Indices of the nearest points in the population matrix.
+
+        Examples
+        --------
+        In the following example, we construct a NearestNeighbors
+        class from an array representing our data set and ask who's
+        the closest point to [1,1,1]
+        """
+        return self.__nn_cpu.kneighbors(X=X, **kwargs)
+
+    def _kneighbors_gpu(self, X, **kwargs):
+        """
+        Find the K-neighbors of a point using GPU.
+
+        Returns indices of and distances to the neighbors of each point.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_queries, n_features), \
+            or (n_queries, n_indexed) if metric == 'precomputed', default=None
+            The query point or points.
+            If not provided, neighbors of each indexed point are returned.
+            In this case, the query point is not considered its own neighbor.
+
+        n_neighbors : int, default=None
+            Number of neighbors required for each sample. The default is the
+            value passed to the constructor.
+
+        return_distance : bool, default=True
+            Whether or not to return the distances.
+
+        Returns
+        -------
+        neigh_dist : ndarray of shape (n_queries, n_neighbors)
+            Array representing the lengths to points, only present if
+            return_distance=True.
+
+        neigh_ind : ndarray of shape (n_queries, n_neighbors)
+            Indices of the nearest points in the population matrix.
+        """
+        if self.__nn_gpu is None:
+            raise NotImplementedError("GPU is not supported")
+        return self.__nn_gpu.kneighbors(X=X, **kwargs)
+
+
+class KNeighborsClassifier(Fit, Predict):
+    """Classifier implementing the k-nearest neighbors vote.
+
+    Read more in the :ref:`User Guide <classification>`.
+
+    Parameters
+    ----------
+    n_neighbors : int, default=5
+        Number of neighbors to use by default for :meth:`kneighbors` queries.
+
+    weights : {'uniform', 'distance'}, callable or None, default='uniform'
+        Weight function used in prediction.  Possible values:
+
+        - 'uniform' : uniform weights.  All points in each neighborhood
+          are weighted equally.
+        - 'distance' : weight points by the inverse of their distance.
+          in this case, closer neighbors of a query point will have a
+          greater influence than neighbors which are further away.
+        - [callable] : a user-defined function which accepts an
+          array of distances, and returns an array of the same shape
+          containing the weights.
+
+        Refer to the example entitled
+        :ref:`sphx_glr_auto_examples_neighbors_plot_classification.py`
+        showing the impact of the `weights` parameter on the decision
+        boundary.
+
+    algorithm : {'auto', 'ball_tree', 'kd_tree', 'brute'}, default='auto'
+        Algorithm used to compute the nearest neighbors:
+
+        - 'ball_tree' will use :class:`BallTree`
+        - 'kd_tree' will use :class:`KDTree`
+        - 'brute' will use a brute-force search.
+        - 'auto' will attempt to decide the most appropriate algorithm
+          based on the values passed to :meth:`fit` method.
+
+        Note: fitting on sparse input will override the setting of
+        this parameter, using brute force.
+
+    leaf_size : int, default=30
+        Leaf size passed to BallTree or KDTree.  This can affect the
+        speed of the construction and query, as well as the memory
+        required to store the tree.  The optimal value depends on the
+        nature of the problem.
+
+    p : float, default=2
+        Power parameter for the Minkowski metric. When p = 1, this is equivalent
+        to using manhattan_distance (l1), and euclidean_distance (l2) for p = 2.
+        For arbitrary p, minkowski_distance (l_p) is used. This parameter is expected
+        to be positive.
+
+    metric : str or callable, default='minkowski'
+        Metric to use for distance computation. Default is "minkowski", which
+        results in the standard Euclidean distance when p = 2. See the
+        documentation of `scipy.spatial.distance
+        <https://docs.scipy.org/doc/scipy/reference/spatial.distance.html>`_ and
+        the metrics listed in
+        :class:`~sklearn.metrics.pairwise.distance_metrics` for valid metric
+        values.
+
+        If metric is "precomputed", X is assumed to be a distance matrix and
+        must be square during fit. X may be a :term:`sparse graph`, in which
+        case only "nonzero" elements may be considered neighbors.
+
+        If metric is a callable function, it takes two arrays representing 1D
+        vectors as inputs and must return one value indicating the distance
+        between those vectors. This works for Scipy's metrics, but is less
+        efficient than passing the metric name as a string.
+
+    metric_params : dict, default=None
+        Additional keyword arguments for the metric function.
+
+    n_jobs : int, default=None
+        The number of parallel jobs to run for neighbors search.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+        Doesn't affect :meth:`fit` method.
+
+    Attributes
+    ----------
+    classes_ : array of shape (n_classes,)
+        Class labels known to the classifier
+
+    effective_metric_ : str or callble
+        The distance metric used. It will be same as the `metric` parameter
+        or a synonym of it, e.g. 'euclidean' if the `metric` parameter set to
+        'minkowski' and `p` parameter set to 2.
+
+    effective_metric_params_ : dict
+        Additional keyword arguments for the metric function. For most metrics
+        will be same with `metric_params` parameter, but may also contain the
+        `p` parameter value if the `effective_metric_` attribute is set to
+        'minkowski'.
+
+    n_features_in_ : int
+        Number of features seen during :term:`fit`.
+
+        .. versionadded:: 0.24
+
+    feature_names_in_ : ndarray of shape (`n_features_in_`,)
+        Names of features seen during :term:`fit`. Defined only when `X`
+        has feature names that are all strings.
+
+        .. versionadded:: 1.0
+
+    n_samples_fit_ : int
+        Number of samples in the fitted data.
+
+    outputs_2d_ : bool
+        False when `y`'s shape is (n_samples, ) or (n_samples, 1) during fit
+        otherwise True.
+
+    See Also
+    --------
+    RadiusNeighborsClassifier: Classifier based on neighbors within a fixed radius.
+    KNeighborsRegressor: Regression based on k-nearest neighbors.
+    RadiusNeighborsRegressor: Regression based on neighbors within a fixed radius.
+    NearestNeighbors: Unsupervised learner for implementing neighbor searches.
+
+    Notes
+    -----
+    See :ref:`Nearest Neighbors <neighbors>` in the online documentation
+    for a discussion of the choice of ``algorithm`` and ``leaf_size``.
+
+    .. warning::
+
+       Regarding the Nearest Neighbors algorithms, if it is found that two
+       neighbors, neighbor `k+1` and `k`, have identical distances
+       but different labels, the results will depend on the ordering of the
+       training data.
+
+    https://en.wikipedia.org/wiki/K-nearest_neighbor_algorithm
+
+    Examples
+    --------
+    >>> X = [[0], [1], [2], [3]]
+    >>> y = [0, 0, 1, 1]
+    >>> from sklearn.neighbors import KNeighborsClassifier
+    >>> neigh = KNeighborsClassifier(n_neighbors=3)
+    >>> neigh.fit(X, y)
+    KNeighborsClassifier(...)
+    >>> print(neigh.predict([[1.1]]))
+    [0]
+    >>> print(neigh.predict_proba([[0.9]]))
+    [[0.666... 0.333...]]
+    """
+    def __init__(self, n_neighbors=5, weights="uniform", algorithm="auto",
+                 leaf_size=30, p=2, metric="minkowski", metric_params=None,
+                 n_jobs=None):
+        """ Constructor of the class KNeighborsClassifier. """
+        self.__knn_cpu = KNeighborsClassifier_CPU(n_neighbors=n_neighbors,
+                                                  weights=weights,
+                                                  algorithm=algorithm,
+                                                  leaf_size=leaf_size,
+                                                  p=p,
+                                                  metric=metric,
+                                                  metric_params=metric_params,
+                                                  n_jobs=n_jobs)
+
+        if is_gpu_supported():
+            self.__knn_gpu = KNeighborsClassifier_GPU(n_neighbors=n_neighbors,
+                                                      weights=weights,
+                                                      algorithm=algorithm,
+                                                      metric=metric)
+        else:
+            self.__knn_gpu = None
+
+    def _fit_cpu(self, X, y=None, **kwargs):
+        """
+        Fit the k-nearest neighbors estimator from the training dataset using
+        CPU only.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features) or \
+                (n_samples, n_samples) if metric='precomputed'
+            Training data.
+
+        y : Ignored
+            Not used, present for API consistency by convention.
+
+        Returns
+        -------
+        self : NearestNeighbors
+            The fitted nearest neighbors estimator.
+        """
+        return self.__knn_cpu.fit(X=X, y=y)
+
+    def _fit_gpu(self, X, y=None, **kwargs):
+        """
+        Fit the k-nearest neighbors estimator from the training dataset using
+        GPU only.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features) or \
+                (n_samples, n_samples) if metric='precomputed'
+            Training data.
+
+        y : Ignored
+            Not used, present for API consistency by convention.
+
+        Returns
+        -------
+        self : NearestNeighbors
+            The fitted nearest neighbors estimator.
+        """
+        if self.__knn_gpu is None:
+            raise NotImplementedError("GPU is not supported")
+        return self.__knn_gpu.fit(X=X, **kwargs)
+
+    def _predict_cpu(self, X, **kwargs):
+        """
+        Predict the class labels for the provided dataset using CPU only.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_queries, n_features), \
+                or (n_queries, n_indexed) if metric == 'precomputed', or None
+            Test samples. If `None`, predictions for all indexed points are
+            returned; in this case, points are not considered their own
+            neighbors.
+
+        Returns
+        -------
+        y : ndarray of shape (n_queries,) or (n_queries, n_outputs)
+            Class labels for each data sample.
+        """
+        return self.__knn_cpu.predict(X=X)
+
+    def _predict_gpu(self, X, y=None, **kwargs):
+        """
+        Predict the class labels for the provided dataset using GPU only.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_queries, n_features), \
+                or (n_queries, n_indexed) if metric == 'precomputed', or None
+            Test samples. If `None`, predictions for all indexed points are
+            returned; in this case, points are not considered their own
+            neighbors.
+
+        Returns
+        -------
+        y : ndarray of shape (n_queries,) or (n_queries, n_outputs)
+            Class labels for each data sample.
+        """
+        if self.__knn_gpu is None:
+            raise NotImplementedError("GPU is not supported")
+        return self.__knn_gpu.predict(X=X, **kwargs)
---
+++ tests/ml/neighbors/test_neighbors.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+import unittest
+
+import numpy as np
+
+try:
+    import cupy as cp
+except ImportError:
+    pass
+
+from mock import Mock, patch
+from sklearn.datasets import make_blobs
+
+from dasf.ml.neighbors import KNeighborsClassifier, NearestNeighbors
+from dasf.utils.funcs import is_gpu_supported
+from dasf.utils.types import (
+    is_cpu_array,
+    is_gpu_array,
+)
+
+
+class TestNearestNeighbors(unittest.TestCase):
+    def setUp(self):
+        self.size = 10
+        self.centers = 3
+        random_state = 42
+
+        self.X, self.y, self.centroids = make_blobs(n_samples=self.size,
+                                                    centers=self.centers,
+                                                    n_features=2,
+                                                    return_centers=True,
+                                                    random_state=random_state)
+
+        self.dists = np.array([[0, 3], [1, 3], [2, 7], [3, 1], [4, 9],
+                               [5, 9], [6, 5], [7, 2], [8, 7], [9, 5]])
+
+    def test_kmeans_cpu(self):
+        nn = NearestNeighbors(n_neighbors=2)
+
+        nn._fit_cpu(self.X)
+
+        idxs, dists = nn._kneighbors_cpu(X=self.X)
+
+        self.assertTrue(is_cpu_array(idxs))
+        self.assertTrue(is_cpu_array(dists))
+
+        self.assertTrue(np.array_equal(dists, self.dists, equal_nan=True))
+
+    @unittest.skipIf(not is_gpu_supported(),
+                     "not supported CUDA in this platform")
+    def test_kmeans_gpu(self):
+        nn = NearestNeighbors(n_neighbors=2)
+
+        cp_X = cp.asarray(self.X)
+
+        nn._fit_gpu(cp_X)
+
+        idxs, dists = nn._kneighbors_gpu(X=self.X)
+
+        self.assertTrue(is_gpu_array(idxs))
+        self.assertTrue(is_gpu_array(dists))
+
+        self.assertTrue(np.array_equal(dists, self.dists, equal_nan=True))
+
+
+class TestKNeighborsClassifier(unittest.TestCase):
+    def setUp(self):
+        self.size = 1000
+        self.centers = 3
+        random_state = 42
+
+        self.X, self.y, self.centroids = make_blobs(n_samples=self.size,
+                                                    centers=self.centers,
+                                                    n_features=2,
+                                                    return_centers=True,
+                                                    random_state=random_state)
+
+        self.dists = np.array([[0, 3], [1, 3], [2, 7], [3, 1], [4, 9],
+                               [5, 9], [6, 5], [7, 2], [8, 7], [9, 5]])
+
+    def __match_randomly_labels_created(self, y1, y2):
+        y2 = (y2 * -1) - 1
+
+        for i in range(len(y1)):
+            if y2[i] < 0:
+                y2[y2 == y2[i]] = y1[i]
+
+            if not np.any(y2[y2 < 0]):
+                break
+
+        return y1, y2
+
+    def test_kmeans_cpu(self):
+        knn = KNeighborsClassifier(n_neighbors=3)
+
+        knn._fit_cpu(self.X, self.y)
+
+        y = knn._predict_cpu(self.X)
+
+        self.assertTrue(is_cpu_array(y))
+
+        y1, y2 = self.__match_randomly_labels_created(y, self.y)
+
+        self.assertTrue(np.array_equal(y1, y2, equal_nan=True))
+
+    @unittest.skipIf(not is_gpu_supported(),
+                     "not supported CUDA in this platform")
+    def test_kmeans_gpu(self):
+        knn = KNeighborsClassifier(n_neighbors=3)
+
+        cp_X = cp.asarray(self.X)
+        cp_y = cp.asarray(self.y)
+
+        knn._fit_gpu(cp_X, cp_y)
+
+        y = knn._predict_gpu(self.X)
+
+        self.assertTrue(is_gpu_array(y))
+
+        y1, y2 = self.__match_randomly_labels_created(y.get(), self.y)
+
+        self.assertTrue(np.array_equal(y1, y2, equal_nan=True))
